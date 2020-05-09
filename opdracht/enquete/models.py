627720bbf279dd8f54996956e3586829da776d64
Segens <sentexdnb@gmail.com>
2020-05-09 14:51:38 +0200

    Scoreveld aan antwoord model toegevoegd i.p.v vraag

--- opdracht/enquete/models.py
+++ opdracht/enquete/models.py
@@ -24,7 +24,6 @@ class Vraag(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, default=1)
     vraag_tekst = models.CharField(max_length=200)
     vraag_type = models.CharField(max_length=50, choices=TYPES)
-    vraag_score = models.FloatField()
     vraag_meerdere = models.BooleanField(verbose_name='Meerdere juiste antwoorden')
     
     def __str__(self):
@@ -37,6 +36,7 @@ class Antwoord(models.Model):
     vraag = models.ForeignKey(Vraag, on_delete=models.CASCADE)
     antwoord_tekst = models.CharField(max_length=200)
     antwoord_juist = models.BooleanField()
+    antwoord_score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.antwoord_tekst
