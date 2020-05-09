efde1b467ec4ee736528961b693642c88c20cab3
Segens <sentexdnb@gmail.com>
2020-05-09 11:59:14 +0200

    begonnen aan een scoresysteem. Lots to fix

--- opdracht/enquete/views.py
+++ opdracht/enquete/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Quiz
+from .models import Quiz, Antwoord
 
 # Create your views here.
 def index(request):
@@ -19,4 +19,46 @@ def quiz_maken(request, quiz_id):
 def quiz_resultaat(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     vragen_list = quiz.vraag_set.all()
-    return render(request, 'resultaat.html', {})
+    user_antwoorden = []
+    juiste_ids = []
+    max_score = 0.0
+    score = 0.0
+
+    # Loop die alle data uit het post request haalt en deze in een list stopt.
+    for data in request.POST:
+        user_antwoorden.append(data)
+    
+    # Verwijdert de csrftoken uit de list.
+    user_antwoorden.pop(0)
+
+    # Convert alle strings in de lijst naar integers
+    user_antwoorden = list(map(int, user_antwoorden))
+
+    print(user_antwoorden)
+
+    for vraag in vragen_list:
+        for juist_antwoord in vraag.antwoord_set.filter(antwoord_juist=True):
+            juiste_ids.append(juist_antwoord.id)
+
+    print(juiste_ids)
+
+    matches = set(user_antwoorden).intersection(juiste_ids)
+    print(matches)
+
+    for id in juiste_ids:
+        antwoord = get_object_or_404(Antwoord, id=id)
+
+        if antwoord.vraag.vraag_meerdere:
+            max_score += antwoord.vraag.vraag_score / 2
+        else:
+            max_score += antwoord.vraag.vraag_score
+
+    for id in matches:
+        antwoord = get_object_or_404(Antwoord, id=id)
+
+        if antwoord.vraag.vraag_meerdere:
+            score += antwoord.vraag.vraag_score / 2
+        else:
+            score += antwoord.vraag.vraag_score
+
+    return render(request, 'resultaat.html', {'user_score': score, 'max_score': max_score})
